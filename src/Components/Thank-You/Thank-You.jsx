dbc6f768c6cdb8ba0113de7a99b75d9247dfe8c0
AfwaanAnsari <afwaanansari@gmail.com>
2024-08-13 10:07:54 +0530

    Added Checkout, Card, UPI, and ThankYou components

---
+++ src/Components/Thank-You/Thank-You.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import './Thank-You.css'
+
+function ThankYou() {
+    return <div></div>;
+}
+
+export default ThankYou;
---
+++ src/Components/UPI/UPI.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import './UPI.css'
+
+function UPI() {
+    return <div></div>;
+}
+
+export default UPI;
